17ea3b062f68c45a91eed4179d403a3eb194b8bb
jon1games <jonas@saslona.com>
2025-02-16 12:32:51 +0100

    add javsdocs

--- src/main/java/me/gaminglounge/configapi/ConfigAPI.java
+++ src/main/java/me/gaminglounge/configapi/ConfigAPI.java
@@ -1,12 +1,21 @@
 package me.gaminglounge.configapi;
 
-import org.bukkit.plugin.java.JavaPlugin; 
+import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * Main file of the plguin
+ */
 public final class ConfigAPI extends JavaPlugin {
- 
+
+    /**
+     * Instance of the plugin used in other methods to acces logger an other things.
+     */
     public static ConfigAPI INSTANCE;
+    /**
+     * static field to acces LoadConfig.
+     */
     public static LoadConfig CONFIG;
- 
+
     @Override
     public void onLoad() {
         INSTANCE = this;
